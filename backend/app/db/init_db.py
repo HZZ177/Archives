9cb3544ff20e9b234eac928724ebb23b77ef9582
HeShouYi_home <863649918@qq.com>
2025-05-14 00:25:52 +0800

    初始超管账户移除邮箱

--- backend/app/db/init_db.py
+++ backend/app/db/init_db.py
@@ -188,7 +188,6 @@ async def init_db() -> None:
                 # 创建管理员用户
                 admin_user = User(
                     username="admin",
-                    email="admin@examples.com",
                     hashed_password=get_password_hash("admin123"),
                     is_active=True,
                     is_superuser=True
