796a3bac1cb16699fc12ee3672352e7a3fcbc668
HeShouYi_home <863649918@qq.com>
2025-06-09 22:34:34 +0800

    cors新增对npm run preview预览服务器端口的授权

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -21,6 +21,7 @@ class Settings(BaseSettings):
     # CORS设置
     CORS_ORIGINS: List[str] = [
         "http://localhost:3000",
+        "http://localhost:4173",
         "http://localhost:5173",
         "http://localhost:8000"
     ]
